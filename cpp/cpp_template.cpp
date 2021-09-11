c7a5f5d8b405c4d07fe35b8749511ae2f4c0fe62
LiWaiYip <1155115790@link.cuhk.edu.hk>
2021-09-11 21:59:34 +0800

    Update cpp_template.cpp

--- cpp/cpp_template.cpp
+++ cpp/cpp_template.cpp
@@ -1,8 +1,37 @@
+// ====================== Code Template: Begin ======================
+// g++ -std=c++17 -Wshadow -Wall Solution.cpp && ./a.out < Solution.in
 #define watch(x) cout << (#x) << " is " << (x) << endl
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
+int ni() {
+    int x;
+    cin >> x;
+    return x;
+}
+ll nl() {
+    ll x;
+    cin >> x;
+    return x;
+}
+string ns() {
+    string s;
+    cin >> s;
+    return s;
+}
+char nc() {
+    char c;
+    cin >> c;
+    return c;
+}
+// ====================== Code Template: End ======================
+
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
+    int T = ni();
+    for (int t = 1; t <= T; t++) {
+        cout << "Case #" <<  t << ": ";
+    }
 }
