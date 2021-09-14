9b931a256713e7998f7d6a8cb8e70aa9a69f10e8
LiWaiYip <1155115790@link.cuhk.edu.hk>
2021-09-15 06:54:03 +0800

    Update random.cpp

--- cpp/random.cpp
+++ cpp/random.cpp
@@ -29,3 +29,33 @@ string random_bin_str(int n) {
     }
     return ans;
 }
+
+
+void testcase() {
+    auto rand_int = [](int min, int max) {
+        return rand() % (max - min + 1) + min;
+    };
+    auto random_vec = [](int n, int min, int max) {
+        vector<int> ans(n);
+        for (int i = 0; i < n; ++i)
+            ans[i] = rand() % (max - min + 1) + min;
+        return ans;
+    };
+    auto random_mat = [](int h, int w, int min, int max) {
+        vector<vector<int>> mat(h, vector<int>(w));
+        for (int i = 0; i < h; ++i) {
+            for (int j = 0; j < w; ++j) {
+                mat[i][j] = rand() % (max - min + 1) + min;
+            }
+        }
+        return mat;
+    };
+}
+
+int main() {
+    bool debug = true;
+    if (debug) {
+        testcase();
+        return 0;
+    }
+}
