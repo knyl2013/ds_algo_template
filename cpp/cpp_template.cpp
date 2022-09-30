1135909274c517c65bb32a57967487f81704704f
waiyip <lwy123177@gmail.com>
2022-09-30 16:56:19 +0800

    Update cpp_template.cpp

--- cpp/cpp_template.cpp
+++ cpp/cpp_template.cpp
@@ -1,59 +1,23 @@
-// ====================== Code Template: Begin ======================
-// g++ -std=c++17 -Wshadow -Wall Solution.cpp && ./a.out < Solution.in
-#define watch(x) cout << (#x) << " is " << (x) << endl
 #include <bits/stdc++.h>
+#define int long long
 using namespace std;
-using ll = long long;
-int ni() {
-    int x;
-    cin >> x;
-    return x;
-}
-ll nl() {
-    ll x;
-    cin >> x;
-    return x;
-}
-string ns() {
-    string s;
-    cin >> s;
-    return s;
-}
-char nc() {
-    char c;
-    cin >> c;
-    return c;
-}
-template <typename T>
-std::ostream & operator << (std::ostream & os, const std::vector<T>& vec) {
-    for(auto elem : vec)
-        os << elem << " ";
-    return os;
-}
-template <typename T>
-std::ostream & operator << (std::ostream & os, const std::vector<vector<T>>& mat) {
-    cout << "\n";
-    for(auto elem : mat)
-        os<<elem<<"\n";
-    return os;
-}
-template <typename A, typename B>
-std::ostream & operator << (std::ostream & os, const std::map<A, B>& mp) {
-    os << "{\n";
-    for (auto& t : mp)
-        os << t.first << ": "  << t.second << "\n";
-    os << "}";
-    return os;
-}
-// ====================== Code Template: End ======================
 
+int n;
+vector<int> arr;
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    int T = ni();
-    for (int t = 1; t <= T; t++) {
-        int ans = 1;
-        cout << "Case #" << t << ": " << ans << "\n";
-    }
+void solve() {
+  cin >> n;
+  arr = vector<int>(n);
+  for (int& x : arr) cin >> x;
+}
+
+signed main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int T = 1;
+  bool multiple_testcases = false;
+  if (multiple_testcases) cin >> T;
+  for (int t = 1; t <= T; t++) {
+    solve();
+  }
 }
