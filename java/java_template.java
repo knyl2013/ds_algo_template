5a38edf3134a548264fa6554ff385c496110b2c4
waiyip <lwy123177@gmail.com>
2022-10-03 17:27:40 +0800

    Java template

---
+++ java/java_template.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.util.*;
+
+public class java_tempalte {
+  static void solve() {
+
+  }
+
+  static boolean multiple = true;
+  static String task = "";
+  static Kattio io;
+
+  public static void main(String[] args) throws IOException {
+    if (!task.isEmpty()) {
+      io = new Kattio(task);
+    } else {
+      io = new Kattio();
+    }
+    int t = 1;
+    if (multiple) {
+      t = io.nextInt();
+    }
+    for (int i = 0; i < t; i++) {
+      solve();
+    }
+    io.close();
+  }
+
+  static class Kattio extends PrintWriter {
+    private BufferedReader r;
+    private StringTokenizer st;
+
+    // standard input
+    public Kattio() {
+      this(System.in, System.out);
+    }
+
+    public Kattio(InputStream i, OutputStream o) {
+      super(o);
+      r = new BufferedReader(new InputStreamReader(i));
+    }
+
+    // USACO-style file input
+    public Kattio(String problemName) throws IOException {
+      super(problemName + ".out");
+      r = new BufferedReader(new FileReader(problemName + ".in"));
+    }
+
+    // returns null if no more input
+    public String next() {
+      try {
+        while (st == null || !st.hasMoreTokens())
+          st = new StringTokenizer(r.readLine());
+        return st.nextToken();
+      } catch (Exception e) {
+      }
+      return null;
+    }
+
+    public int nextInt() {
+      return Integer.parseInt(next());
+    }
+
+    public double nextDouble() {
+      return Double.parseDouble(next());
+    }
+
+    public long nextLong() {
+      return Long.parseLong(next());
+    }
+  }
+}
