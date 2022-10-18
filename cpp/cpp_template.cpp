7274e0f44bb7850e1b75afa1607098794fd9280d
waiyip <lwy123177@gmail.com>
2022-10-18 09:42:08 +0800

    Update cpp_template.cpp

--- cpp/cpp_template.cpp
+++ cpp/cpp_template.cpp
@@ -11,11 +11,19 @@ void solve() {
   for (int& x : arr) cin >> x;
 }
 
+/* I/O Template */
+void setIO(string task = "") {
+  cin.tie(0)->sync_with_stdio(0);
+  if (!task.empty()) {
+    freopen((task + ".in").c_str(), "r", stdin);
+    freopen((task + ".out").c_str(), "w", stdout);
+  }
+}
+
 signed main() {
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
   int T = 1;
   bool multiple_testcases = false;
+  setIO();
   if (multiple_testcases) cin >> T;
   for (int t = 1; t <= T; t++) {
     solve();
