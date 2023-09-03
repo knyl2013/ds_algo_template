b63777624343502e2a38752042f3fcc0b0ee25c3
aichathub <aichathub@outlook.com>
2023-09-02 21:39:11 -0400

    Fix nCr()

--- python/nCr_nPr.py
+++ python/nCr_nPr.py
@@ -1,21 +1,33 @@
-mod = int(1e9+7)
+mod = int(1e9 + 7)
+nax = int(2e5 + 10)
+
+
 def mul(a: int, b: int) -> int:
-		return (a * b) % mod
+    return (a * b) % mod
+
+
 def pow(a: int, b: int) -> int:
-		if b == 0:
-				return 1
-		if b % 2 == 1:
-				return mul(a, pow(a, b - 1))
-		else:
-				c = pow(a, b // 2)
-				return mul(c, c)
+    if b == 0:
+        return 1
+    if b % 2 == 1:
+        return mul(a, pow(a, b - 1))
+    else:
+        c = pow(a, b // 2)
+        return mul(c, c)
+
+
 def inv(x: int) -> int:
-		return pow(x, mod - 2)
-facts = [1] * (n + 1)
-for i in range(1, n+1):
-		facts[i] = mul(facts[i - 1], i)
-@lru_cache(None)
+    return pow(x, mod - 2)
+
+
+facts = [1] * (nax + 1)
+for i in range(1, nax):
+    facts[i] = mul(facts[i - 1], i)
+
+
 def nPr(n: int, r: int) -> int:
-		return mul(facts[n], inv(facts[n - r]))
-def nCr(n: int, c: int) -> int:
-		return mul(nPr(n, r), facts[r])
+    return mul(facts[n], inv(facts[n - r]))
+
+
+def nCr(n: int, r: int) -> int:
+    return mul(nPr(n, r), inv(facts[r]))
